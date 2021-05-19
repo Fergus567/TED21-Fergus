25f160bed846b16bc5fcacf99ce607608b3f2040
Fergus567 <82346182+Fergus567@users.noreply.github.com>
2021-05-20 09:26:35 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # Fergus Earl
 # Environmental Quiz
 
+'''An environmentally aware quiz on 5 relevant global topics and issues.'''
+
 # imports
 import json
 import random
 import easygui
+from collections import OrderedDict
 
 # functions
 def get_quiz():
+    '''Loads the quiz data from questions.json as a dictionary'''
     with open('questions.json', 'r') as f:
-        return json.load(f) # loads the quiz data from questions.json as a dictionary
+        return json.load(f, object_pairs_hook=OrderedDict) # returns the quiz dictionary and orders it with the collections input
+                                                            #(fixes questions sometimes being out of order on load)
 
 # initial variable declarations
 condition = True
 quiz = get_quiz() 
+
+print(quiz)
 
 # main routine
 if easygui.msgbox("Welcome to the EnviroQuiz!\ncoded and maintained by Fergus Earl", "EnviroQuiz | Welcome") != 'OK':
@@ -22,13 +29,14 @@ if easygui.msgbox("Welcome to the EnviroQuiz!\ncoded and maintained by Fergus Ea
 while condition: # loops the script until the condition is set to false
     score = 0
 
+
     for question in quiz:
         answers = quiz[question]["answers"]
         answer = quiz[question]["answer"]
         fact = quiz[question]["fact"]
 
         random.shuffle(answers) # shuffle the list to create a different order of answers each time
-        
+
         user_answer = easygui.buttonbox(question, "EnviroQuiz | Score {}".format(score), answers)
 
         if user_answer not in answers:
@@ -39,7 +47,6 @@ while condition: # loops the script until the condition is set to false
             statement = 'Correct!'
         else:
             statement = 'Incorrect! The right answer was {}'.format(answer)
-
 
         if easygui.msgbox("{}\n\n{}".format(statement, fact), 'EnviroQuiz | Score {}'.format(score), ('Continue')) != 'Continue':
             exit() # means the close window button was pressed
